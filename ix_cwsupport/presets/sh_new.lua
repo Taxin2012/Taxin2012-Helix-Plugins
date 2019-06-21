17fba1eab542d6ede2a8a5059ebe7b838a0c92f9
Taxin2012 <mrtaxin2012@mail.ru>
2019-06-21 12:15:18 +0300

    Add files via upload

---
+++ ix_cwsupport/presets/sh_new.lua
@@ -0,0 +1,360 @@
+
+PLUGIN.gunData.cw_nen_beretta92fs = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 1,
+	width = 2,
+	height = 1,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_nen_glock17 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 1,
+	width = 2,
+	height = 1,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_ragingbull = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 1,
+	width = 2,
+	height = 1,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_vz61_kry = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 1,
+	width = 3,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_acr68 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_aw_ak12 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_akm = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_ws_mosin = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 5,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_bizongsm = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 3,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_ppsh41 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_rpg7 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 5,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_trg42 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_scorpin_evo3 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_dragnov = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_awm = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_xm1014 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_pkm = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 5,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_fnp45 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 2,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_asval = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 3,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_mp40 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 3,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_dbarrel = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_toz34 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_shawedoff = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_ry_an94 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_l85fix = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_tr09_lr300 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_tr09_lr300 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_shek_browninghp = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 2,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_ry_870 = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
+
+PLUGIN.gunData.cw_gauss_rifle = {
+	holster = {
+		bone = "ValveBiped.Bip01_R_Thigh",
+		ang = Angle(-90, -90,0),
+		pos = Vector(0, 10, -4),
+	},
+	slot = 2,
+	width = 4,
+	height = 2,
+	price = 2000,
+}
