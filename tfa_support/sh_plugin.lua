11b088c7203a9e19256c3cd9e7ed5d7fcaff4dec
Taxin2012 <mrtaxin2012@mail.ru>
2019-11-16 18:15:25 +0300

    TFA Weapons Support
    Please, check those lines inside "sh_tfa_support.lua":
    145, 232, 286, 354, 399

---
+++ tfa_support/sh_plugin.lua
@@ -0,0 +1,33 @@
+
+--
+-- Copyright (C) 2019 Taxin2012
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+
+
+--	Writed by Taxin2012
+--	https://steamcommunity.com/id/Taxin2012/
+
+
+
+local PLUGIN = PLUGIN
+PLUGIN.name = "TFA Support"
+PLUGIN.author = "Taxin2012"
+PLUGIN.description = "Support for TFA Weapons Pack"
+
+
+
+ix.util.Include("sh_tfa_support.lua")
+ix.util.Include("cl_tfa_support.lua")
