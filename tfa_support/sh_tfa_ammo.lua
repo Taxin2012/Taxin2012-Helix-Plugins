5af04f6ccd29e76c684594b934cd30b0bd23d1c6
Taxin2012 <mrtaxin2012@mail.ru>
2020-05-10 18:38:10 +0300

    Update sh_tfa_ammo.lua

--- tfa_support/sh_tfa_ammo.lua
+++ tfa_support/sh_tfa_ammo.lua
@@ -1,6 +1,6 @@
 
 --
--- Copyright (C) 2019 Taxin2012
+-- Copyright (C) 2020 Taxin2012
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
