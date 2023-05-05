de511c4b828ac47aa4c2d4ba5a3cb4d2d9857ca4
Taxin2012 <26491089+Taxin2012@users.noreply.github.com>
2023-05-05 22:41:54 +0300

    Added support for individual weapon files.
    Added support for individual weapon files in schema or plugins.

---
+++ tfa_support/items/weapons/sh_pistol.lua
@@ -0,0 +1,9 @@
+ITEM.name = "Test TFA Pistol"
+ITEM.description = "dasd as dasda s"
+ITEM.model = "models/weapons/w_pistol.mdl"
+ITEM.class = "tfa_glock"
+ITEM.weaponCategory = "sidearm"
+ITEM.width = 2
+ITEM.height = 1
+
+ITEM.useTFASupport = true --Use TFA support plugin
